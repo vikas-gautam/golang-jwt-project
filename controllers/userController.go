4570e707c8b34d3446ff45d634224df8ca58cf1a
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-06-27 10:20:06 +0530

    [Day8]-completed GetUsers func

--- controllers/userController.go
+++ controllers/userController.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -140,7 +141,62 @@ func VerifyPassword(userPassword string, providedPassword string) (bool, string)
 	return check, mesg
 }
 
-func GetUsers() {}
+func GetUsers() gin.HandlerFunc{
+	return func(c *gin.Context){
+		helper.CheckUserType(c, "ADMIN"); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err := nil || recordPerPage <1{
+			recordPerPage = 10
+		}
+
+		page, err1 := strconv.Atoi(c.Query("page"))
+		if err1 != nil || page <1{
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+
+		groupStage := bson.D{{"$group", bson.D{
+			{"_id", bson.D{{"_id", "null"}}}, 
+			{"total_count", bson.D{{"$sum", 1}}}, 
+			{"data", bson.D{{"$push", "$$ROOT"}}}
+		 }}}
+
+		//u could control which data can go to users
+	    projectStage := bson.D{
+			{"project", bson.D{
+				{"_id", 0},
+				{"total_count", 1},
+				{"user_items", bson.D{{"$slice", []interface{}{"data", startIndex, recordPerPage}}}},
+			}}
+		}
+
+result,err := userCollection.Aggregate(ctx, mongo.Pipeline{
+	matchStage, groupStage, projectStage
+})
+
+defer cancel()
+if err != nil{
+	c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing user items"})
+}
+
+var allUsers []bson.M
+if err = result.All(ctx, &allUsers); err != nil{
+	log.Fatal(err)
+}
+c.JSON(http.StatusOK, allUsers[0])
+	}
+
+
+}
 
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
